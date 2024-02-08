8809df5c935575cca3afe8411253a016ff1e944b
Andrew Grant <andrewgrant2009@hotmail.com>
2024-02-08 15:18:26 +0100

    ready to send to github

---
+++ app/posts/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function PostPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+<section>Page for a single Post</section>
+    </main>
+  );
+}
